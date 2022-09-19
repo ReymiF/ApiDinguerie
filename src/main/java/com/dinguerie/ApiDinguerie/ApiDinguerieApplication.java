d1ce933a4a11f083ef0e9c73e23bae3a90d1e815
reymi <remiktb@gmail.com>
2022-09-19 10:33:47 -0700

    Init

---
+++ src/main/java/com/dinguerie/ApiDinguerie/ApiDinguerieApplication.java
@@ -0,0 +1,13 @@
+package com.dinguerie.ApiDinguerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDinguerieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDinguerieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinguerie/ApiDinguerie/ApiDinguerieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinguerie.ApiDinguerie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDinguerieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
